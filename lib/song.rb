7bad41d6a650844f8d91be0ff8c3f5f07508c197
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-05 12:40:11 -0500

    commit tree fef276b185152551a331befec2167ab7ab345922

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,69 @@ class Song
     self.class.all << self
   end
 
+
+  def self.create
+  song = self.new
+  song.save
+  song
+  end
+  
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |song| song.name == name
+      end
+  end
+
+  
+  def  self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+    
+  end
+
+  def self.alphabetical
+    @@all.sort_by do |song| song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    song_parts = filename.split(" - ")
+    artist_name = song_parts[0]
+    song_name = song_parts[1].gsub(".mp3", "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_parts = filename.split(" - ")
+    artist_name = song_parts[0]
+    song_name = song_parts[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
